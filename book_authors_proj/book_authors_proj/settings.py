ba1c08ef2536c50b734e62531871c01e21b20bcc
Hector Zambrano <hzambranoleal@gmail.com>
2021-03-02 20:16:22 -0300

    modificaciones

--- book_authors_proj/book_authors_proj/settings.py
+++ book_authors_proj/book_authors_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fx65ibz8#+wylm*fk%f-5c&^o9lcl_)8cfe&k)=o4g6dv++xns'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-3-15-140-76.us-east-2.compute.amazonaws.com', '3.15.140.76']
 
 
 # Application definition
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")	
